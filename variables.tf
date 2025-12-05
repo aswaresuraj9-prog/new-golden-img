8f7ef896966806f7d92f28be1dee04ccd96ad82f
aswaresuraj9-prog <aswaresuraj9@gmail.com>
2025-12-05 23:34:21 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,35 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.113"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-    azapi = {
-      source  = "Azure/azapi"
-      version = "~> 1.13"
-    }
-  }
+variable "location" {
+  description = "Primary Azure region"
+  type        = string
+  default     = "eastus"
 }
 
+variable "image_definition_name" {
+  description = "Name of the image definition"
+  type        = string
+  default     = "golden-image"
+}
 
+variable "image_version" {
+  description = "Version of the image"
+  type        = string
+  default     = "1.0.0"
+}
 
+variable "publisher" {
+  description = "Image publisher"
+  type        = string
+  default     = "MicrosoftWindowsDesktop"
+}
 
+variable "offer" {
+  description = "Image offer"
+  type        = string
+  default     = "windows-11"
+}
 
+variable "sku" {
+  description = "Image SKU"
+  type        = string
+  default     = "win11-22h2-pro"
+}
