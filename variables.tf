eee94c5b18775283081b2799fd112b77f5a05184
aswaresuraj9-prog <aswaresuraj9@gmail.com>
2025-12-05 23:18:48 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,48 @@
+variable "rg_name" {
+  description = "The name of the resource group"
+  default     = "new-image-gold-rg"
+}
 
-variable "rg_name" { default = "new-image-gold-rg" }
-variable "location" { default = "eastus" }
-variable "replication_regions" { default = ["eastus", "centralindia"] }
-variable "gallery_name" { default = "sig_golden" }
-variable "image_definition_name" { default = "win11-avd-golden" }
-variable "image_version" { default = "1.0.0" }
+variable "location" {
+  description = "Azure region for resources"
+  default     = "eastus"
+}
 
-variable "source_publisher" { default = "MicrosoftWindowsDesktop" }
-variable "source_offer" { default = "Windows-11" }
-variable "source_sku" { default = "win11-24h2-avd" }
+variable "replication_regions" {
+  description = "List of regions for image replication"
+  default     = ["eastus", "centralindia"]
+}
+
+variable "gallery_name" {
+  description = "Name of the Shared Image Gallery"
+  default     = "sig_golden"
+}
+
+variable "image_definition_name" {
+  description = "Name of the image definition"
+  default     = "win11-avd-golden"
+}
+
+variable "image_version" {
+  description = "The version of the image"
+  default     = "1.0.0"
+}
+
+variable "source_publisher" {
+  description = "The publisher of the base image"
+  default     = "MicrosoftWindowsDesktop"
+}
+
+variable "source_offer" {
+  description = "The offer of the base image"
+  default     = "Windows-11"
+}
+
+variable "source_sku" {
+  description = "The SKU of the base image"
+  default     = "win11-24h2-avd"
+}
+
+
+
+
