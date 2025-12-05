86cce7b56f60491e10b5d1b100f564716b05c277
aswaresuraj9-prog <aswaresuraj9@gmail.com>
2025-12-05 23:33:59 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,47 +1,22 @@
-variable "rg_name" {
-  description = "The name of the resource group"
-  default     = "new-image-gold-rg"
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.113"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 1.13"
+    }
+  }
 }
 
-variable "location" {
-  description = "Azure region for resources"
-  default     = "eastus"
-}
-
-variable "replication_regions" {
-  description = "List of regions for image replication"
-  default     = ["eastus", "centralindia"]
-}
-
-variable "gallery_name" {
-  description = "Name of the Shared Image Gallery"
-  default     = "sig_golden"
-}
-
-variable "image_definition_name" {
-  description = "Name of the image definition"
-  default     = "win11-avd-golden"
-}
-
-variable "image_version" {
-  description = "The version of the image"
-  default     = "1.0.0"
-}
-
-variable "source_publisher" {
-  description = "The publisher of the base image"
-  default     = "MicrosoftWindowsDesktop"
-}
-
-variable "source_offer" {
-  description = "The offer of the base image"
-  default     = "Windows-11"
-}
-
-variable "source_sku" {
-  description = "The SKU of the base image"
-  default     = "win11-24h2-avd"
-}
 
 
 
